4a6a83805966be6425d3511961e00284459cb4ff
DESKTOP-FD4CQOG\Gizmo <andrea_v@post.cz>
2021-05-02 17:16:15 +0200

    projekt bez funkce zobrazování pouze jednoho kvízu

--- kviz.js
+++ kviz.js
@@ -22,90 +22,146 @@ let hadanka = {
     spravnaOdpoved: 'Mončičák'
 };
 
+poleOtazek.forEach(function(generovaniOtazek){
 
+    //bily ramecek
+    let kviz = document.createElement('div');
+    kviz.className = 'kviz';
 
-//bily ramecek
-let kviz = document.createElement('div');
-kviz.className = 'kviz';
+    //rozvržení bílého rámečku
+    let obsah = document.createElement('div');
+    obsah.className = 'obsah';
 
-//rozvržení bílého rámečku
-let obsah = document.createElement('div');
-obsah.className = 'obsah';
+    //Růžové číslo
+    let poradi = document.createElement('h2');
+    poradi.id = 'poradi';
+    poradi.innerHTML = generovaniOtazek.cislo;
 
-//Růžové číslo
-let poradi = document.createElement('h2');
-poradi.id = 'poradi';
-poradi.innerHTML = hadanka.cislo;
+    //Hlavní otázka
+    let dotaz = document.createElement('h2');
+    dotaz.id = 'otazka';
+    dotaz.innerHTML = generovaniOtazek.otazka;
 
-//Hlavní otázka
-let dotaz = document.createElement('h2');
-dotaz.id = 'otazka';
-dotaz.innerHTML = hadanka.otazka;
+    //obrazek
+    let obrazek = document.createElement('img');
+    obrazek.id = 'obrazek';
+    obrazek.className = 'foto';
+    obrazek.src = generovaniOtazek.foto;
 
-//obrazek
-let obrazek = document.createElement('img');
-obrazek.id = 'obrazek';
-obrazek.className = 'foto';
-obrazek.src = hadanka.foto;
+    //moznosti
+    let moznosti = document.createElement('div');
+    moznosti.id = 'moznosti';
+    let odpovedi = document.createElement('ul');
+    odpovedi.id = 'odpovedi';
+    odpovedi.className = 'odpovedi';
+    let pocetOdpovedi = generovaniOtazek.moznost.length;
 
-//moznosti
-let moznosti = document.createElement('div');
-moznosti.id = 'moznosti';
-let odpovedi = document.createElement('ul');
-odpovedi.id = 'odpovedi';
-odpovedi.className = 'odpovedi';
-let pocetOdpovedi = hadanka.moznost.length;
-
-//vygeneruje to všechny možnosti
-hadanka.moznost.forEach(function (vec){
+    //vygeneruje to všechny možnosti
+    generovaniOtazek.moznost.forEach(function (vec){
     let li = document.createElement('li');
     odpovedi.appendChild(li);
     li.innerHTML += vec;
+    });
+
+    //zde mi bohuzel nefunguje odebírání druhého a třetího kvízu (pro začátek jsem chtěla jen ten druhý)
+        //let cisloOdebraneho = 1;
+        //let odebrani = document.querySelector('.kviz');
+        //odebrani.parentNode.removeChild(odebrani[cisloOdebraneho]);
+    
+
+    //funkce na kliknutí
+    odpovedi.onclick = function (event) {
+        let odpovedElement = event.target;
+        console.log("Kliknul jsi na " + odpovedElement.innerText);
+        poleOdpovedi.push(odpovedElement.innerText);
+        console.log(poleOdpovedi);
+
+        let odebraniPredchoziho = document.querySelector('.kviz');
+        odebraniPredchoziho.parentNode.removeChild(odebraniPredchoziho);
+    
+        //ukazat vysledek
+        if (poleOdpovedi.length >= 3) {
+            ukazVysledek();
+        }
+
+        //cisloOdebraneho++;
+        
+    }
+
+    //implementace do stránky
+    kviz.appendChild(poradi);
+    kviz.appendChild(dotaz);
+    kviz.appendChild(obsah);
+
+    obsah.appendChild(obrazek);
+    obsah.appendChild(moznosti);
+
+    moznosti.appendChild(odpovedi);
+
+    //dát tabulku do těla stránky
+    let tabulka = document.querySelector("body");
+    tabulka.appendChild(kviz);
+
+
 });
 
-//funkce na kliknutí
-odpovedi.onclick = function (event) {
-    let odpovedElement = event.target;
-    console.log("Kliknul jsi na " + odpovedElement.innerText);
-    poleOdpovedi.push(odpovedElement.innerText);
-    console.log(poleOdpovedi);
+function ukazVysledek () {
+    console.log("odpovedi jsou zadány");
 
+    //bily ramecek
+    let kviz = document.createElement('div');
+    kviz.className = 'kviz';
+
+    //Růžový nadpis
+    let poradi = document.createElement('h2');
+    poradi.id = 'poradi';
+    poradi.innerHTML = "Tvoje hodnocení";
+
+    kviz.appendChild(poradi);
+
+    let cisloOtazky = 0;
+    let pocetSpravnychOdpovedi = 0;
+
+    poleOtazek.forEach(function(porovnavaniOdpovedi){
+        let dotaz = document.createElement('h3');
+        dotaz.innerHTML = porovnavaniOdpovedi.otazka;
+
+        let vyhodnoceni = document.createElement('div');
+        vyhodnoceni.innerHTML = "Tvoje odpověď je: " + poleOdpovedi[cisloOtazky];
+        
+
+        let spravneOdpovedi = document.createElement('div');
+        spravneOdpovedi.innerHTML = "Správná odpoveď na otázku je: " 
+            + poleOtazek[cisloOtazky].spravnaOdpoved;
+        
+
+
+        let dobraOdpoved = document.createElement('div');
+        if (poleOdpovedi[cisloOtazky] === poleOtazek[cisloOtazky].spravnaOdpoved) {
+            dobraOdpoved.innerHTML = "Jsi nejlepší, toto je SPRÁVNĚ!";
+            pocetSpravnychOdpovedi++;
+        }
+
+        kviz.appendChild(dotaz);
+        kviz.appendChild(vyhodnoceni);
+        kviz.appendChild(spravneOdpovedi);
+        kviz.appendChild(dobraOdpoved);
+
+        cisloOtazky++;
+    });
+
+    let uspesnost = document.createElement('h3');
+    uspesnost.id = 'poradi';
+    uspesnost.innerHTML = "Počet správných odpovědí je " + pocetSpravnychOdpovedi + " ze " + poleOtazek.length;
+    kviz.appendChild(uspesnost);
+
+    //dát tabulku do těla stránky
+    let tabulka = document.querySelector("body");
+    tabulka.appendChild(kviz);
+
+   
+
+
+
+    
 }
-
-
-//const tlacitka = document.querySelectorAll('.odpovedi');
-//tlacitka.forEach((tlacitko) => {
-//    tlacitko.addEventListener('click', (udalost) => {
-//        console.log('ahoj' + udalost.target.innerText);
-//    });
-//});
-
-//nebo takto
-//document.querySelector('#odpovedi').addEventListener('click', ulozOdpoved);
-
-
-
-let zmacknuti = document.querySelectorAll('.odpovedi'); //všechny obrázky mi to vytáhne
-zmacknuti.forEach((zmacknuti) => { //zavolám nad všema obrázkama funkci
-    zmacknuti.addEventListener('click', ulozOdpoved);
-});
-function ulozOdpoved (event) {
-    let odpovedElement = event.target;
-    console.log("Kliknul jsi" + odpovedElement);
-}
-
-
-
-//implementace do stránky
-kviz.appendChild(poradi);
-kviz.appendChild(dotaz);
-kviz.appendChild(obsah);
-
-obsah.appendChild(obrazek);
-obsah.appendChild(moznosti);
-
-moznosti.appendChild(odpovedi);
-
-//dát tabulku do těla stránky
-let tabulka = document.querySelector("body");
-tabulka.appendChild(kviz);
